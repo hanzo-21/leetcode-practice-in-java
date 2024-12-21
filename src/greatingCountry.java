a897cf1fb24717f6b7bc9d09d8e8af4cd73b52a8
aayush rana magar <realnaramagaraayush@gmail.com>
2024-12-21 21:23:40 +0545

    tried nedd to comelepe

---
+++ src/greatingCountry.java
@@ -0,0 +1,12 @@
+public class greatingCountry {
+    public static String greetUser(String country) {
+        switch (country){
+            case: "English"
+
+
+            default:
+                break;
+
+        }
+    }
+}
