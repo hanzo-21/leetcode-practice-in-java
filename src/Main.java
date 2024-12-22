adf2e2af966b1df595003a3643b07060a83877b9
aayush rana magar <realnaramagaraayush@gmail.com>
2024-12-22 20:56:20 +0545

    doing camel case

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        String h = smilyFace.replaceSmiley("Have a nice day :)");
+        String h = smilyFace.replaceSmiley("hello world");
 
         System.out.println(h);
 
--- src/camelCase.java
+++ src/camelCase.java
@@ -1,2 +1,18 @@
 public class camelCase {
+    public static String convertToCamelCase(String str) {
+        String sub1 , newString = "";
+
+        for(int i=0 ;i < str.length();i++ ){
+            sub1 = str.substring(i,i+1);
+            if(sub1.compareTo(" ") == 0){
+                i++;
+                sub1 = str.substring(i,i+1);
+                sub1 = sub1.toUpperCase();
+                newString = newString.concat(sub1);
+            }
+            newString = newString.concat(sub1);
+        }
+        return newString;
+    }
 }
+
