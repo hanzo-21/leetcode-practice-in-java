1a95f3d329a0e1f06555ac4f339ff2cdc449f88c
aayush rana magar <realnaramagaraayush@gmail.com>
2025-01-30 10:38:44 +0545

    update to practices

--- src/Main.java
+++ src/Main.java
@@ -4,52 +4,9 @@ import java.util.Scanner;
 class Main {
 
     // method to find the first and last occurrences of an element in an array
-    public static int[] findOccurrences(int[] array, int element) {
 
-        // store index of first occurrence
-        int firstOccurrence = -1;
-
-        // store index of first occurrence
-        int lastOccurrence = -1;
-
-        // iterate over the array to find the first occurrence of the element
-        for (int i =0 ; i < array.length ;i++){
-            if(element == array[i] && firstOccurrence == -1){
-                firstOccurrence = i;
-            }
-            if(element == array[i]){
-                lastOccurrence =i;
-            }
-        }
-
-                // iterate over the array in reverse to find the last occurrence of the element
-
-
-        // return an array containing the first and last occurrences of the element
-        return new int[] {firstOccurrence, lastOccurrence};
-    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // get input from the user
-        int[] array = new int[6];
-
-        // populate the array with user input
-        for (int i = 0; i < array.length; i++) {
-            array[i] = scanner.nextInt();
-        }
-
-        // element to search for
-        int element = 5;
-
-        // find the first and last occurrences of the element
-        int[] occurrences = findOccurrences(array, element);
-
-        // print the first and last occurrences of the element
-        if (occurrences[0] != -1) {
-            System.out.println(occurrences[0]);
-            System.out.println(occurrences[1]);
-        }
+       System.out.println(needForSpeed.calculateTimeSaved(60,80,240));
     }
 }
---
+++ src/needForSpeed.java
@@ -0,0 +1,6 @@
+public class needForSpeed {
+    public static double calculateTimeSaved(double speed1, double speed2, double distance) {
+        return ((distance/speed1) - (distance/speed2));
+
+    }
+}
